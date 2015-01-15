27c11a5301e43de21d0714b5555d11595fe02c59
andres-root <andres.root.coder@gmail.com>
2015-01-15 11:21:35 -0500

    adding controllers

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -1,6 +1,7 @@
 var FinanceApp = angular.module('FinanceApp', []);
 
 FinanceApp.controller('FinanceController', function ($scope) {
+	$scope.title = 'Stock Market';
 	$scope.stocks = [
 		{"company": "google", "name": "goog", "value": 1000.0},
 		{"company": "facebook", "name": "fb", "value": 900.0},
