67e8279fe15a4562b35d0ed8df00bf5dfb4e37ad
andres-root <andres.root.coder@gmail.com>
2015-01-15 11:57:42 -0500

    counted filtered items

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -7,6 +7,6 @@ FinanceApp.controller('FinanceController', function ($scope) {
 	$scope.stocks = [
 		{"company": "google", "name": "goog", "value": 1000.0},
 		{"company": "facebook", "name": "fb", "value": 900.0},
-		{"company": "twitter", "name": "tw", "value": 800.0},
+		{"company": "twitter", "name": "tw", "value": 800.0}
 	];
 });
