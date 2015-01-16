99107e1ac1761ee695ffc4188fed2fe9e2c104cb
andres-root <andres.root.coder@gmail.com>
2015-01-16 16:51:11 -0500

    creating routes

---
+++ content/js/app.js
@@ -0,0 +1,13 @@
+var financeApp = angular.module('financeApp', ['ngRoute', 'financeControllers']);
+
+financeApp.config(['$routeProvider',
+	function ($routeProvider) {
+		$routeProvider.when('/stocks', 
+			{templateUrl: 'templates/stocks.html',
+			 controller: 'stocksController'}
+		).when('/stocks/:stockId',
+			{templateUrl: 'templates/stock.html',
+			 controller: 'stockController'}
+		)
+	}
+]);
