68cbf4216a3fc186c6e81ac984a1e862e6d3edc0
andres-root <andres.root.coder@gmail.com>
2015-01-16 11:08:44 -0500

    adding data.json to make xhr requests

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -4,10 +4,6 @@ var FinanceApp = angular.module('FinanceApp', []);
 
 FinanceApp.controller('FinanceController', function ($scope) {
 	$scope.title = 'Stock Market';
-	$scope.stocks = [
-		{"company": "google", "name": "goog", "value": 1000.0, "image": "google.png"},
-		{"company": "facebook", "name": "fb", "value": 900.0, "image": "facebook.png"},
-		{"company": "twitter", "name": "tw", "value": 800.0, "image": "twitter.jpg"}
-	];
+	$scope.stocks = [];
 	$scope.orderProp = '-value';
 });
