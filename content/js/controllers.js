54db9632fe7ecb678afb0ed2832b04a6a0fa12e5
andres-root <andres.root.coder@gmail.com>
2015-01-16 09:40:07 -0500

    added images

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -7,6 +7,6 @@ FinanceApp.controller('FinanceController', function ($scope) {
 	$scope.stocks = [
 		{"company": "google", "name": "goog", "value": 1000.0, "image": "google.png"},
 		{"company": "facebook", "name": "fb", "value": 900.0, "image": "facebook.png"},
-		{"company": "twitter", "name": "tw", "value": 800.0, "image": "twitter.png"}
+		{"company": "twitter", "name": "tw", "value": 800.0, "image": "twitter.jpg"}
 	];
 });
