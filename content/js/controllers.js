17e2f520fbeb6d2b6a9031da4fe84b0654cac5a7
andres-root <andres.root.coder@gmail.com>
2015-01-16 09:38:06 -0500

    added images

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -5,8 +5,8 @@ var FinanceApp = angular.module('FinanceApp', []);
 FinanceApp.controller('FinanceController', function ($scope) {
 	$scope.title = 'Stock Market';
 	$scope.stocks = [
-		{"company": "google", "name": "goog", "value": 1000.0},
-		{"company": "facebook", "name": "fb", "value": 900.0},
-		{"company": "twitter", "name": "tw", "value": 800.0}
+		{"company": "google", "name": "goog", "value": 1000.0, "image": "google.png"},
+		{"company": "facebook", "name": "fb", "value": 900.0, "image": "facebook.png"},
+		{"company": "twitter", "name": "tw", "value": 800.0, "image": "twitter.png"}
 	];
 });
