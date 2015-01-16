a853b370b78ab7f002d068459332e2b99793f17e
andres-root <andres.root.coder@gmail.com>
2015-01-16 11:21:58 -0500

    bugfix

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -4,7 +4,7 @@ var FinanceApp = angular.module('FinanceApp', []);
 
 FinanceApp.controller('FinanceController', function ($scope, $http) {
 	$scope.title = 'Stock Market';
-	$http.get('data.json').sucess(function (data) {
+	$http.get('data.json').success(function (data) {
 		$scope.stocks = data;
 	});
 	$scope.orderProp = '-value';
