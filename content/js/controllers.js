5beb1856aea6c91f31ca0bf7927310f5a5b2cab6
andres-root <andres.root.coder@gmail.com>
2015-01-17 20:01:59 -0500

    added list and detail controllers for stock

--- content/js/controllers.js
+++ content/js/controllers.js
@@ -1,10 +1,17 @@
 
 var financeControllers = angular.module('financeControllers', []);
 
-financeControllers.controller('StocksController', function ($scope, $http) {
-	$scope.title = 'Stock Market';
-	$http.get('data.json').success(function (data) {
-		$scope.stocks = data;
-	});
-	$scope.orderProp = '-value';
-});
+financeControllers.controller('StocksController', ['$scope', '$http', 
+	function ($scope, $http) {
+		$http.get('data.json').success(function (data){
+			$scope.stocks = data;
+		});
+		$scope.orderProp = '-value';
+	}
+]);
+
+financeControllers.controller('StockController', ['$scope' '$routeParams',
+	function ($scope, $routeParams) {
+		$scope.stockId = $routeParams.stockId;
+	}
+]);
