bceb34edf62dfa35e6bedadeba30e64ccbb7718a
andres-root <andres.root.coder@gmail.com>
2015-01-19 09:42:19 -0500

    update2

--- content/config.rb
+++ content/config.rb
@@ -8,7 +8,6 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
-project_path = "content"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
