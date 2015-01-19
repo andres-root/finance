cae4ccb7fbe494a8b8d3ccf70e41af7a9efb1229
andres-root <andres.root.coder@gmail.com>
2015-01-19 09:12:43 -0500

    added config-dist file for sass

---
+++ content/config-dist.rb
@@ -0,0 +1,22 @@
+# General Frontend Compass Settings
+# 2015-01-19
+# @andresroot
+
+# With relative_assets these are not required
+http_images_path = "/content/img"
+http_fonts_path	= "/content/fonts"
+http_path = "/content"
+css_dir = "dist/css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+project_path = "content"
+#asset_cache_buster = false
+
+# For source maps
+# sass_options = { :debug_info => true }
+relative_assets = false
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
